423a49a6db4c9fb71c17f9a30f206ee14bb61928
huangweiliang <huangweiliang@mucfc.com>
2019-11-03 12:17:45 +0800

    feat: use okHttp replace leanCloud

--- app/src/main/java/com/vch/stockapp/BondManager.java
+++ app/src/main/java/com/vch/stockapp/BondManager.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
@@ -24,19 +25,26 @@ import androidx.core.app.NotificationCompat;
 import cn.leancloud.AVCloud;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
 
 /**
  * 可转债管理类
- * 后台配置在leancloud的云函数中。云函数存储在github中，地址为
- * https://github.com/hwldzh/python-getting-started.git
  * @author VC_H
  * @date 2019-10-26
  */
 public class BondManager {
     public static final String TAG = "BondManager";
 
+    /**
+     * 后台配置在leancloud的云函数中。云函数存储在github中，地址为
+     * https://github.com/hwldzh/python-getting-started.git
+     * @param listener
+     */
     public static void getBondMsg(final IBondListener listener) {
         // 构建传递给服务端的参数字典
         Map<String, Object> dicParameters = new HashMap<String, Object>();
@@ -52,65 +60,7 @@ public class BondManager {
                 Log.d(TAG, "get bond msg success");
                 if (object instanceof String) {
                     String data = (String) object;
-                    final String regex = "(\\{\".*?\\})";
-                    final Pattern pattern = Pattern.compile(regex);
-                    final Matcher matcher = pattern.matcher(data);
-
-                    Calendar calendar = Calendar.getInstance();
-                    int year = calendar.get(Calendar.YEAR);
-                    int month = calendar.get(Calendar.MONTH) + 1;
-                    int day = calendar.get(Calendar.DAY_OF_MONTH);
-                    List<BondModel> bondList = new ArrayList<>();
-                    boolean hasFound = false;
-                    while (matcher.find()) {
-                        String groupData = matcher.group(0);
-                        if (groupData == null) {
-                            continue;
-                        }
-                        try {
-                            JSONObject groupDataObj = new JSONObject(groupData);
-                            //债券代码
-                            String bondcode = groupDataObj.optString("BONDCODE");
-                            //债券简称
-                            String sname = groupDataObj.optString("SNAME");
-                            //申购日期
-                            String startdate = groupDataObj.optString("STARTDATE");
-                            //申购代码
-                            String correscode = groupDataObj.optString("CORRESCODE");
-                            //正股代码
-                            String swapscode = groupDataObj.optString("SWAPSCODE");
-                            //正股简称
-                            String securityshortname = groupDataObj.optString("SECURITYSHORTNAME");
-                            String[] dateArray = startdate.split("-");
-                            if (dateArray.length != 3) {
-                                Log.e(TAG, "startdate parse error：" + startdate);
-                                continue;
-                            }
-                            String yearStr = dateArray[0];
-                            String monthStr = dateArray[1];
-                            String dayStr = dateArray[2].substring(0, 2);
-                            startdate = yearStr + "-" + monthStr + "-" + dayStr;
-
-                            if (Integer.parseInt(yearStr) == year && Integer.parseInt(monthStr) == month
-                                    && Integer.parseInt(dayStr) == day) {
-                                hasFound = true;
-                                BondModel bondModel = new BondModel(bondcode, sname, startdate, correscode
-                                        , swapscode, securityshortname);
-                                bondList.add(bondModel);
-                                Log.d(TAG, "bingo day：" + startdate);
-                                Log.d(TAG, "债券代码：" + bondcode + "; 债券简称："
-                                        + sname + "; 申购日期：" + startdate + "; 申购代码：" + correscode
-                                        + "; 正股代码："+swapscode + "; 正股简称：" + securityshortname);
-
-                            } else { //由于可转债时间的顺序是按时间逆序的，因此一旦找不到则后面也再找不到了
-                                if (hasFound) {
-                                    break;
-                                }
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
+                    List<BondModel> bondList = getBondMsgInGroup(data);
                     if (listener != null) {
                         listener.onBondMsgSuccess(bondList);
                     }
@@ -130,6 +80,69 @@ public class BondManager {
             @Override
             public void onComplete() {}
         });
+    }
+
+    private static List<BondModel> getBondMsgInGroup(String allBondMsg) {
+        final String regex = "(\\{\".*?\\})";
+        final Pattern pattern = Pattern.compile(regex);
+        final Matcher matcher = pattern.matcher(allBondMsg);
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        List<BondModel> bondList = new ArrayList<>();
+        boolean hasFound = false;
+        while (matcher.find()) {
+            String groupData = matcher.group(0);
+            if (groupData == null) {
+                continue;
+            }
+            try {
+                JSONObject groupDataObj = new JSONObject(groupData);
+                //债券代码
+                String bondcode = groupDataObj.optString("BONDCODE");
+                //债券简称
+                String sname = groupDataObj.optString("SNAME");
+                //申购日期
+                String startdate = groupDataObj.optString("STARTDATE");
+                //申购代码
+                String correscode = groupDataObj.optString("CORRESCODE");
+                //正股代码
+                String swapscode = groupDataObj.optString("SWAPSCODE");
+                //正股简称
+                String securityshortname = groupDataObj.optString("SECURITYSHORTNAME");
+                String[] dateArray = startdate.split("-");
+                if (dateArray.length != 3) {
+                    Log.e(TAG, "startdate parse error：" + startdate);
+                    continue;
+                }
+                String yearStr = dateArray[0];
+                String monthStr = dateArray[1];
+                String dayStr = dateArray[2].substring(0, 2);
+                startdate = yearStr + "-" + monthStr + "-" + dayStr;
+
+                if (Integer.parseInt(yearStr) == year && Integer.parseInt(monthStr) == month
+                        && Integer.parseInt(dayStr) == day) {
+                    hasFound = true;
+                    BondModel bondModel = new BondModel(bondcode, sname, startdate, correscode
+                            , swapscode, securityshortname);
+                    bondList.add(bondModel);
+                    Log.d(TAG, "bingo day：" + startdate);
+                    Log.d(TAG, "债券代码：" + bondcode + "; 债券简称："
+                            + sname + "; 申购日期：" + startdate + "; 申购代码：" + correscode
+                            + "; 正股代码："+swapscode + "; 正股简称：" + securityshortname);
+
+                } else { //由于可转债时间的顺序是按时间逆序的，因此一旦找不到则后面也再找不到了
+                    if (hasFound) {
+                        break;
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return bondList;
     }
 
     public static void sendBondMsg(Context context, String content, String channelId, int notificationId) {
@@ -156,7 +169,7 @@ public class BondManager {
     }
 
     @TargetApi(Build.VERSION_CODES.O)
-    public static void createNotificationChannel(Context context, String channelId, String channelName, int importance) {
+    private static void createNotificationChannel(Context context, String channelId, String channelName, int importance) {
         NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(
                 NOTIFICATION_SERVICE);
@@ -164,5 +177,53 @@ public class BondManager {
             return;
         }
         notificationManager.createNotificationChannel(channel);
+    }
+
+    /**
+     * 利用OkHttp拉取网页数据
+     * @param listener
+     */
+    public static void getBondMsgFromOkHttp(final IBondListener listener) {
+        final OkHttpClient okHttpClient = new OkHttpClient();
+        final Request request = new Request.Builder().url("http://data.eastmoney.com/xg/kzz/default.html").build();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Response response;
+                try {
+                    response = okHttpClient.newCall(request).execute();
+                    if (response.code() == 200) {
+                        ResponseBody responseBody = response.body();
+                        if (responseBody == null) {
+                            if (listener != null) {
+                                listener.onBondMsgFailed("拉取数据失败. responseBody return null!");
+                            }
+                        } else {
+                            String responseStr = responseBody.string();
+                            final String regex = "defjson:(.*\\})";
+                            final Pattern pattern = Pattern.compile(regex);
+                            final Matcher matcher = pattern.matcher(responseStr);
+
+                            while (matcher.find()) {
+                                String allBondMsg = matcher.group(0);
+                                List<BondModel> bondList = getBondMsgInGroup(allBondMsg);
+                                if (listener != null) {
+                                    listener.onBondMsgSuccess(bondList);
+                                }
+                            }
+                        }
+                    } else {
+                        if (listener != null) {
+                            listener.onBondMsgFailed("拉取数据失败. responseCode=" + response.code());
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    if (listener != null) {
+                        listener.onBondMsgFailed("网络请求失败. errorMsg=" + e.toString());
+                    }
+                }
+            }
+        }).start();
     }
 }
--- app/src/main/java/com/vch/stockapp/MainActivity.java
+++ app/src/main/java/com/vch/stockapp/MainActivity.java
@@ -1,15 +1,12 @@
 package com.vch.stockapp;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
@@ -27,31 +24,41 @@ public class MainActivity extends AppCompatActivity {
 
     private void getBondMsg() {
         progressBarView.setVisibility(View.VISIBLE);
-        BondManager.getBondMsg(new IBondListener() {
+        BondManager.getBondMsgFromOkHttp(new IBondListener() {
             @Override
-            public void onBondMsgSuccess(List<BondModel> bondList) {
-                progressBarView.setVisibility(View.GONE);
-                if (bondList == null) {
-                    return;
-                }
-                if (bondList.size() == 0) {
-                    bondTextView.setText("今日无申购指标");
-                    return;
-                }
-                StringBuilder bondMsgBuilder = new StringBuilder();
-                for (BondModel bondModel : bondList) {
-                    String bondMsg = "债券简称：" + bondModel.getSname() + " 申购代码：" + bondModel.getCorrescode();
-                    bondMsgBuilder.append(bondMsg).append("\n");
-                    BondManager.sendBondMsg(MainActivity.this,
-                            bondMsg, bondModel.getCorrescode(), Integer.parseInt(bondModel.getCorrescode()));
-                }
-                bondTextView.setText(bondMsgBuilder.toString());
+            public void onBondMsgSuccess(final List<BondModel> bondList) {
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBarView.setVisibility(View.GONE);
+                        if (bondList == null) {
+                            return;
+                        }
+                        if (bondList.size() == 0) {
+                            bondTextView.setText("今日无申购指标");
+                            return;
+                        }
+                        StringBuilder bondMsgBuilder = new StringBuilder();
+                        for (BondModel bondModel : bondList) {
+                            String bondMsg = "债券简称：" + bondModel.getSname() + " 申购代码：" + bondModel.getCorrescode();
+                            bondMsgBuilder.append(bondMsg).append("\n");
+                            BondManager.sendBondMsg(MainActivity.this,
+                                    bondMsg, bondModel.getCorrescode(), Integer.parseInt(bondModel.getCorrescode()));
+                        }
+                        bondTextView.setText(bondMsgBuilder.toString());
+                    }
+                });
             }
 
             @Override
-            public void onBondMsgFailed(String errMsg) {
-                progressBarView.setVisibility(View.GONE);
-                bondTextView.setText("出错了.出错信息为：" + errMsg);
+            public void onBondMsgFailed(final String errMsg) {
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBarView.setVisibility(View.GONE);
+                        bondTextView.setText("出错了.出错信息为：" + errMsg);
+                    }
+                });
             }
         });
     }
